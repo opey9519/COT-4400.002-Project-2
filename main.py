49fd287dc2202d52bac7201941da91e98eb922bb
Gavin Wilson <gmanwilson@icloud.com>
2026-03-30 12:04:43 -0400

    add: main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+# Project 2 code
+
+# MergeSort
+def MergeSort():
+    pass
+
+# Dynamic Programming
+
+
+def DP():
+    pass
+
+# Greedy Algorithm
+
+
+def Greedy():
+    pass
+
+# Contains tests & function calls
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
